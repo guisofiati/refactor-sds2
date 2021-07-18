11c15ca8a082215d36ab7b151669fe3ba2c7b81f
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-07-18 12:31:08 -0300

    useEffect

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
+
 type Props = {
     message: string,
     number: Number
 }
 
 function Hello ({ message, number }: Props) {
+
+    useEffect(() => {
+        console.log('componente iniciou! ')
+    }, []);
+
     return (
         <h1> {message} {number}</h1>
     );
